e5c4ad99d7987b8ae476df6dd4410e05e48a67ba
Kittiphob Saibunyang <epxt199@gmail.com>
2025-02-06 10:40:07 +0700

    แก้ชื่อเป็นรหัสนิสิต ตำแหน่ง1

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
 
-    <div className="text-3xl font-bold bg-teal-300 flex justify-center">1.header</div>
+    <div className="text-3xl font-bold bg-teal-300 flex justify-center">1.66022826</div>
     <div className="text-3xl font-bold bg-teal-400 flex justify-center">2.header</div>
     <div className="text-3xl font-bold bg-teal-500 flex justify-center">3.header</div></>
 
