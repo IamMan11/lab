a2f7102bded2d5e233584eed3e9e92bdccb4bf5b
Kittiphob Saibunyang <epxt199@gmail.com>
2025-02-06 10:39:19 +0700

    up next.js

---
+++ my-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+
+    <div className="text-3xl font-bold bg-teal-300 flex justify-center">1.header</div>
+    <div className="text-3xl font-bold bg-teal-400 flex justify-center">2.header</div>
+    <div className="text-3xl font-bold bg-teal-500 flex justify-center">3.header</div></>
+
+  );
+}
